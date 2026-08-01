87407762685cbc09de3fcfe1e70b25a53b3b655a
Leon <leoncorcoran65@outlook.com>
2026-08-01 19:07:15 +0100

    Script that takes a user input of 3 items and takes stock count and once reaches 0, displays out of stock message. Using True as the argument forces the user to break loop

---
+++ shopping.py
@@ -0,0 +1,23 @@
+
+inventory = {
+    "apple" : 3,
+    "banana" : 2,
+    "pear" : 1
+}
+# Using True as an argument in the loop, forces user to manually break the loop, will continue to ask for item to buy otherwise or display out of stock if everything is gone.
+while True:
+    item = input("What would you like to buy? (Apple. Banana, Pear) ")
+    item = item.lower()
+    if item == "quit" or item == "Quit":
+        break
+    if item not in inventory:
+        print("Not a recognised item")
+    elif inventory[item] == 0:
+        print(f"{item.capitalize()}s are out of stock.")
+    else:
+        inventory[item] -= 1
+        print(f"Stock: {inventory[item]}")
+        
+        
+        
+    
