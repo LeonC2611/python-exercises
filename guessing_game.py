4215daad1dd2b05ee1f1bf35d19357be4adfcf25
Leon <leoncorcoran65@outlook.com>
2026-08-04 01:04:20 +0100

    Changes made to add a list that tracks wrong guesses and force user to choose a new number. Secret number also updated to be numbers up to 100

--- guessing_game.py
+++ guessing_game.py
@@ -1,14 +1,21 @@
 import random
 
-secret_number = random.randint(1, 4)
+secret_number = random.randint(1, 100)
 
 guess = int(input("Guess the secret number: "))
+#list to track numbers that have already been guessed and forces user to choose different number
+past_guesses = []
 #takes a number and returns whether it's too high or low compared to secret number
 while guess != secret_number:
-    if guess > secret_number:
+    if guess in past_guesses:
+        guess = int(input("You've already guessed that, try again: "))
+    elif guess not in past_guesses:
+        past_guesses.append(guess)
+        if guess > secret_number:
           guess = int(input("Too high, guess again: "))
-    elif guess < secret_number:
+        elif guess < secret_number:
           guess = int(input("Too low, guess again: "))
     
+
 if guess == secret_number:
     print("Well done")
