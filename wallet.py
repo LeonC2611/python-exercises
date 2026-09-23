09ed9c9520fa91641506210e88987840f2afde3c
Leon <leoncorcoran65@outlook.com>
2026-09-23 22:51:47 +0100

    Added transfer method to transfer amount from one wallet to another and catch any invalid wallets with an error

--- wallet.py
+++ wallet.py
@@ -32,14 +32,25 @@ class Wallet:
         else:
             self._balance -= amount
 
-    # balance property to use later if needed
+    # Method to transfer amount from one wallet to another when another wallet instance is created. If not, will raise an error.
+    def transfer(self, other_wallet, amount):
+        if not isinstance(other_wallet, Wallet):
+            raise ValueError("Other wallet doesn't exist.")
+        else:
+            self.withdraw(amount)
+            other_wallet.deposit(amount)
+
+    # Balance property to use later if needed
     @property
     def balance(self):
         return self._balance
 
 
-wallet = Wallet()
+leon = Wallet()
+luke = Wallet()
 
-wallet.deposit(4)
-wallet.withdraw(3)
-print(wallet)
+leon.deposit(10)
+leon.transfer(luke, 7)
+
+print(leon)
+print(luke)
